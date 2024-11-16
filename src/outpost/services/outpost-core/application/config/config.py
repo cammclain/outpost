471941773fb9378e97f96c02642f039f5c0fc219
Cam <cammclain@protonmail.com>
2024-11-17 08:48:20 +1300

    add pydantic config validation

--- src/outpost/services/outpost-core/application/config/config.py
+++ src/outpost/services/outpost-core/application/config/config.py
@@ -2,22 +2,22 @@ from __future__ import annotations
 import os
 from dotenv import load_dotenv
 
+from pydantic import BaseSettings
 
+from pydantic import Field, AnyHttpUrl, ValidationError
+from pydantic.config import BaseSettings
 
+class Settings(BaseSettings):
+    # Environment variables with types and optional default values
+    REDIS_HOST: str
+    REDIS_PORT: int
+    DATABASE_URL: AnyHttpUrl = Field(default="sqlite:///outpost.db")
+    SESSION_SECRET_KEY: str
 
-
-class Settings:
-    # load the environment variables
-    load_dotenv()
-
-    # get the environment variables
-    REDIS_HOST = os.getenv("REDIS_HOST")
-    REDIS_PORT  = (os.getenv("REDIS_PORT"))
-
-    # the database url
-    DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///outpost.db")
-
-
+    class Config:
+        # Specify the `.env` file location
+        env_file = ".env"
+        env_file_encoding = "utf-8"
 
 
 settings = Settings()
