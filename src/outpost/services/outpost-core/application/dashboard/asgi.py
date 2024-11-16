229ce10297cbee2231d3a71ebe8542f9a5767bbf
Cam <cammclain@protonmail.com>
2024-11-17 08:49:15 +1300

    update asgi values for middleware stuff

--- src/outpost/services/outpost-core/application/dashboard/asgi.py
+++ src/outpost/services/outpost-core/application/dashboard/asgi.py
@@ -7,12 +7,50 @@ from pathlib import Path
 from litestar import Litestar
 from litestar.contrib.jinja import JinjaTemplateEngine
 from litestar.template.config import TemplateConfig
-## the main application object
-app = Litestar(
-    route_handlers=[],
-    template_config=TemplateConfig(
-        engine=JinjaTemplateEngine(),
-        directory=Path("templates"),
-    ),
-    
-)
+from litestar.static_files import StaticFilesConfig
+from .middleware.auth import AuthenticationMiddleware
+
+from litestar.middleware.session.server_side import ServerSideSessionBackend, ServerSideSessionConfig
+from config.config import settings
+from redis.asyncio import Redis
+
+ASSETS_DIR = Path("static")
+
+
+def create_app() -> Litestar:
+
+    # create the redis client
+    redis_host = settings.REDIS_HOST
+    redis_port = settings.REDIS_PORT
+    redis: Redis = Redis(host=redis_host, port=redis_port)
+
+    # create the session backend
+    session_backend = ServerSideSessionBackend(config=ServerSideSessionConfig(secret_key=settings.SESSION_SECRET_KEY, max_age=3600))
+
+    ## the main application object
+    app = Litestar(
+        # add the routes
+        route_handlers=[
+        ],
+
+        # add the authentication middleware
+        middleware=[
+            AuthenticationMiddleware,
+            session_backend
+        ],
+
+        # add the static files config
+        static_files_config=[
+            StaticFilesConfig(path="/static", directories=[ASSETS_DIR], html_mode=True)
+        ],
+
+        # add the template config
+        template_config=TemplateConfig(
+            engine=JinjaTemplateEngine(),
+            directory=Path("templates"),
+        ),
+
+    )
+    return app
+
+app = create_app()
