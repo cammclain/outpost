1fab60ae48232efb0928eabdbdad29c3e1a4557b
Cam <cammclain@protonmail.com>
2024-11-16 15:38:23 +1300

    set up jinja templating

--- src/outpost/services/outpost-core/application/dashboard/asgi.py
+++ src/outpost/services/outpost-core/application/dashboard/asgi.py
@@ -3,11 +3,16 @@
 # pulls everything together for the the full outpost core application
 
 from __future__ import annotations
-
+from pathlib import Path
 from litestar import Litestar
-
-
+from litestar.contrib.jinja import JinjaTemplateEngine
+from litestar.template.config import TemplateConfig
 ## the main application object
 app = Litestar(
     route_handlers=[],
+    template_config=TemplateConfig(
+        engine=JinjaTemplateEngine(),
+        directory=Path("templates"),
+    ),
+    
 )
